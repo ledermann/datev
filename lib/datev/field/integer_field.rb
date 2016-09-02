e6cd609fe00d5c95d263e5a60ee93ec488d21360
Georg Ledermann <mail@georg-ledermann.de>
2016-09-02 15:51:55 +0200

    Add min/max validation for IntegerField

--- lib/datev/field/integer_field.rb
+++ lib/datev/field/integer_field.rb
@@ -4,12 +4,22 @@ module Datev
       options[:limit]
     end
 
+    def maximum
+      options[:maximum]
+    end
+
+    def minimum
+      options[:minimum]
+    end
+
     def validate!(value)
       super
 
       if value
         raise ArgumentError.new("Value given for field '#{name}' is not an Integer") unless value.is_a?(Integer)
         raise ArgumentError.new("Value '#{value}' for field '#{name}' is too long") if limit && value.to_s.length > limit
+        raise ArgumentError.new("Value '#{value}' for field '#{name}' is too large") if maximum && value > maximum
+        raise ArgumentError.new("Value '#{value}' for field '#{name}' is too small") if minimum && value < minimum
       end
     end
 
--- spec/datev/field/integer_field_spec.rb
+++ spec/datev/field/integer_field_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Datev::IntegerField do
-  subject { Datev::IntegerField.new 'foo', :limit => 3, :required => true }
+  subject { Datev::IntegerField.new 'foo', :limit => 3, :minimum => 5, :maximum => 500, :required => true }
 
   describe :validate! do
     it "should accept valid value" do
@@ -12,8 +12,19 @@ describe Datev::IntegerField do
       [ '123', 100.5, true ].each do |value|
         expect { subject.validate!(value) }.to raise_error(ArgumentError, "Value given for field 'foo' is not an Integer")
       end
-      expect { subject.validate!(1000)    }.to raise_error(ArgumentError, "Value '1000' for field 'foo' is too long")
       expect { subject.validate!(nil)     }.to raise_error(ArgumentError, "Value for field 'foo' is required")
+    end
+
+    it "should fail for too long value" do
+      expect { subject.validate!(1000)    }.to raise_error(ArgumentError, "Value '1000' for field 'foo' is too long")
+    end
+
+    it "should fail for too small value" do
+      expect { subject.validate!(4)       }.to raise_error(ArgumentError, "Value '4' for field 'foo' is too small")
+    end
+
+    it "should fail for too large value" do
+      expect { subject.validate!(501)     }.to raise_error(ArgumentError, "Value '501' for field 'foo' is too large")
     end
   end
 
